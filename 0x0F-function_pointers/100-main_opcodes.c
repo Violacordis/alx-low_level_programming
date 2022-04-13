f22a1c6d271c560f17eeac740e8dfcd1d3850a2e
Violacordis <ikemviolacordis@gmail.com>
2022-04-13 09:59:54 -0700

    modified

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,37 +9,33 @@
  */
 int main(int argc, char *argv[])
 {
-  int bytes, index;
-  int (*address)(int, char **) = main;
-  unsigned char opcode;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-  bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-  if (bytes < 0)
-    {
-      printf("Error\n");
-      exit(2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-  for (index = 0; index < bytes; index++)
-    {
-      opcode = *(unsigned char *)address;
-      printf("%.2x", opcode);
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+		if (index == bytes - 1)
+			continue;
+		address++;
+	}
 
-      if (index == bytes - 1)
-	continue;
-      printf(" ");
-
-      address++;
-    }
-
-  printf("\n");
-
-  return (0);
+	printf("\n");
+	return (0);
 }
